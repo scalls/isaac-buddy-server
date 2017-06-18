581e43df71296cac0f215d1840a22e531a1ccfc4
scalls <scalls@rpi.edu>
2017-06-18 12:02:45 -0400

    trying something new

--- main.js
+++ main.js
@@ -10,20 +10,41 @@ var apiai = require("apiai");
 var key = apiai("2657996f7a2541ec827362de2c09cf8d");
 
 const express = require('express')()
+const bodyParser = require('body-parser')
 
-express.get('/', function(req, res) {
-  res.send('Hello World!')
-})
+express.use(bodyParser.json())
 
 express.post('/', function(req, res) {
-  console.log('req: ' + JSON.stringify(req))
-  console.log('req.params: ' + JSON.stringify(req.params))
-  console.log('req.query: ' + JSON.stringify(req.query))
-  res.send({
-    'speech': 'Derp.',
-    'source': 'Statman',
-    'displayText': 'Derp.'
-  })
+
+  console.log('Received POST request')
+
+  try {
+
+    if (req.body) {
+
+      var requestBody = req.body
+      if (requestBody.result.action) {
+        console.log('Received action: ' + requestBody.result.action)
+      }
+
+    }
+
+    res.send({
+      'speech': 'Derp.',
+      'source': 'Statman',
+      'displayText': 'Derp.'
+    })
+
+  } catch(err) {
+
+    console.error('Cannot process request', err)
+    res.status(400).json({
+      status: {
+        code: 400,
+        errorType: err.message
+      }
+    })
+  }
 })
 
 express.listen(process.env.PORT || 3000, function() {

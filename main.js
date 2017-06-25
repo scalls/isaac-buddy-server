e168ebdd5cb584481ace1fbf6aebc7ee6d2bbc97
scalls@rpi.edu <scalls@rpi.edu>
2017-06-25 10:20:19 -0400

    removed circular print

--- main.js
+++ main.js
@@ -35,7 +35,6 @@ express.post('/', (req, res) => {
 
   try {
     console.log('Trying stuff...')
-    console.log('Received request: ' + JSON.stringify(req))
     if (req.body) {
       var requestBody = req.body
       console.log('Request body: ' + JSON.stringify(requestBody))
